5c43e84b6734a7db54ddb4cbb47976e87437c98d
Git-hub-gk <161137899+Git-hub-gk@users.noreply.github.com>
2024-04-29 14:32:07 +0530

    add on

--- Java_projects/Test1.java
+++ Java_projects/Test1.java
@@ -1,5 +1,5 @@
 class Test1 {
     public static void main(String[] args) {
-        System.out.println("Aur Bahi Kya Haal Hain!");
+        System.out.println("Aur Bahi kaya phad raha hai tu, sir kuch nahi phaddda rhe.!!!!");
     }
 }
