dbace5a8c2fb6078915e9578d442690fb6bbc31f
Git-hub-gk <161137899+Git-hub-gk@users.noreply.github.com>
2024-04-29 16:19:11 +0530

    Update Test1.java

--- Java_projects/Test1.java
+++ Java_projects/Test1.java
@@ -1,6 +1,10 @@
 class Test1 {
     public static void main(String[] args) {
         String x = "Hello.!!!!";
+        int y = 20;
         System.out.println(x);
+        System.out.println(y);
+        y=y+20;
+        System.out.println(y);
     }
 }
